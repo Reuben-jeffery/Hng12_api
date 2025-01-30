26202aacfa7b15867063dac11b6d59bd495a2cfe
Reuben-jeffery <reubenjeffery47@gmail.com>
2025-01-30 16:33:54 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/appi', methods=['GET'])
+def get_info():
+    
+    email = "reubenjeffery47@gmail.com"
+    
+    current_time = datetime.utcnow().isoformat() + 'Z'
+    
+    github_url = "https://github.com/Reuben-jeffery/Hng12_api.git"
+    
+    return jsonify({
+        "email": email,
+        "current_time": current_time,
+        "github_url": github_url
+    })
+    
+if __name__ == '__main__':
+    app.run(debug=True)   
