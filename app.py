0934c3904cb328d60e06b9fb278b3b264d4bbe27
Reuben-jeffery <reubenjeffery47@gmail.com>
2025-01-31 11:14:37 +0100

    update

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_info():
     
     email = "reubenjeffery47@gmail.com"
     
-    current_time = datetime.now(timezone.utc).isoformat()
+    current_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     
     github_url = "https://github.com/Reuben-jeffery/Hng12_api"
     
